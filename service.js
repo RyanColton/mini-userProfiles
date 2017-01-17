3dfbe05dbb44ce11a8e5fb118b3a9b7c75d7748f
RyanColton <ryan.m.colton@gmail.com>
2017-01-17 14:35:53 -0700

    Finished Project Part 2

--- service.js
+++ service.js
@@ -1,28 +1,17 @@
-angular.module('userProfiles').service('mainService', function(){
-  var data = [{
-    "id": 0,
-    "first_name": "george",
-    "last_name": "bluth",
-    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg"
-},
-{
-    "id": 1,
-    "first_name": "lucille",
-    "last_name": "bluth",
-    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"
-},
-{
-    "id": 2,
-    "first_name": "oscar",
-    "last_name": "bluth",
-    "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg"
-}]
-
-this.getUsers = function(){
-  return data;
-}
+angular.module('userProfiles').service('mainService', function($http) {
+  var data;
+  this.getUsers = function() {
+    return $http({
+      method: 'GET',
+      url: 'https://reqres.in/api/users?page=1'
+    }).then(function(response){
+      data = response.data.data;
+      return response;
+    })
+  }
 
 this.toggleFavorite = function(index){
-  data[index].isfavorite = !data[index].isfavorite;
+  console.log("this.data", data)
+  data[index - 1].isfavorite = !data[index - 1].isfavorite;
 }
 })
